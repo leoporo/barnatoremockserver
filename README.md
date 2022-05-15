6e24e191d3f160c16657815364313fbb33328362
Leonis Poroshtica <leonis.poroshtica@hotmail.com>
2022-05-15 19:04:39 +0200

    add database generator

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Mock Server per Barnatore
 
-Lanso json serverin: json-server --watch db.json
-Ne nje terminal tjeter shkruaj komanden: node index
+Lanso skripten index.js me ane te komandes: node index
+Kjo do te gjeneroje databazen heren e pare. Pastaj ristartoje skripten e njejte, dhe do te filloje te randomizoje vlerat e sensorave cdo 5 sekonda.
+Ne nje terminal tjeter shkruaj komanden: json-server --watch db.json
 
-E para servon databazen, e dyta i ndryshon cdo 5 sekonda parametrat me vlera rastesore.
+Njesite e perdorura: celsius (temperature), perqindje (lageshti relative), dhe lux (drite).
 
-Ne VS Code, kur ta hapni db.json, mund ta formatoni me komanden Alt+Shift+f.
+Ne VS Code, kur ta hapni db.json, mund ta formatoni me komanden alt+shift+f.
--- index.js
+++ index.js
@@ -1,17 +1,118 @@
 const fs  = require("fs");
 
-setInterval(randomizeSensorData, 5000);
+if (fs.existsSync("db.json")) {
+    console.log("db.json already exists. Randomizing sensor data...");
+    setInterval(randomizeSensorData, 5000);
+}
+else {
+    console.log("db.json not found. Generating new database.");
+    generateDatabase();
+    console.log("Database generated. Restart this script.");
+}
 
 function randomizeSensorData() {
     const data = JSON.parse(fs.readFileSync("db.json"));
 
     for (let i = 0; i < data.pharmacies.length; i++) {
         for (let j = 0; j < data.pharmacies[i].sensors.length; j++) {
-            data.pharmacies[i].sensors[j].temp = Math.floor(Math.random()*10);
-            data.pharmacies[i].sensors[j].hum = Math.floor(Math.random()*10);
-            data.pharmacies[i].sensors[j].light = Math.floor(Math.random()*10);
+            data.pharmacies[i].sensors[j].temp = Math.floor(Math.random()*30);
+            data.pharmacies[i].sensors[j].hum = Math.floor(Math.random()*100);
+            data.pharmacies[i].sensors[j].light = Math.floor(Math.random()*100000);
         }
     }
 
     fs.writeFileSync("db.json", JSON.stringify(data));
+}
+
+function generateDatabase() {
+    const db = {
+        pharmacies: [
+            {
+                name: "farmacia1",
+                address: "adresa1",
+                sensors: [
+                    {
+                        name: "sensor1",
+                        type: "depo",
+                        location: {
+                            lat: 1,
+                            long: 1
+                        },
+                        temp: 0,
+                        hum: 0,
+                        light: 0
+                    },
+                    {
+                        name: "sensor2",
+                        type: "transport",
+                        location: {
+                            lat: 1,
+                            long: 1
+                        },
+                        temp: 0,
+                        hum: 0,
+                        light: 0
+                    }
+                ]
+            },
+            {
+                name: "farmacia2",
+                address: "adresa2",
+                sensors: [
+                    {
+                        name: "sensor1",
+                        type: "depo",
+                        location: {
+                            lat: 1,
+                            long: 1
+                        },
+                        temp: 0,
+                        hum: 0,
+                        light: 0
+                    },
+                    {
+                        name: "sensor2",
+                        type: "transport",
+                        location: {
+                            lat: 1,
+                            long: 1
+                        },
+                        temp: 0,
+                        hum: 0,
+                        light: 0
+                    }
+                ]
+            },
+            {
+                name: "farmacia3",
+                address: "adresa3",
+                sensors: [
+                    {
+                        name: "sensor1",
+                        type: "depo",
+                        location: {
+                            lat: 1,
+                            long: 1
+                        },
+                        temp: 0,
+                        hum: 0,
+                        light: 0
+                    },
+                    {
+                        name: "sensor2",
+                        type: "transport",
+                        location: {
+                            lat: 1,
+                            long: 1
+                        },
+                        temp: 0,
+                        hum: 0,
+                        light: 0
+                    }
+                ]
+            }
+        ]
+    };
+
+    fs.writeFileSync("db.json", JSON.stringify(db));
 }
